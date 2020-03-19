e3b3f5ad7d7ad22ab72f32709a3c156ca012dba9
Ann-Marie Kemp <aedalenb@gmail.com>
2020-03-19 15:40:45 -0400

    pushed with webpack config

--- webpack/base.js
+++ webpack/base.js
@@ -34,7 +34,7 @@ module.exports = {
       WEBGL_RENDERER: JSON.stringify(true)
     }),
     new HtmlWebpackPlugin({
-      template: "./index.html"
+      template: "./public/index.html"
     })
   ]
 };
