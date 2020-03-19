76275b2e29a31bba26a9eee834be76f240d88b44
Ann-Marie Kemp <aedalenb@gmail.com>
2020-03-19 15:14:54 -0400

    changed some file structure and added game.js file

---
+++ public/Level1.js
@@ -0,0 +1,10 @@
+export default class Level1 extends Phaser.Scene {
+  constructor() {
+    super({ key: "Level1" });
+  }
+  preload() {}
+  createImageBitmap() {}
+  update() {
+    console.log("working");
+  }
+}
