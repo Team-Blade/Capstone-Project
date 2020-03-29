ebdbb07427a42e8031799bb0267189d0f152a259
Ann-MarieKemp <aedalenb@gmail.com>
2020-03-29 13:15:26 -0700

    added big devinition to index.js playermovement function

--- index.js
+++ index.js
@@ -79,15 +79,15 @@ io.on("connection", socket => {
   socket.on("disconnect", () => {
     console.log("user disconnected");
     if (socket.roomId) {
-      console.log('before, rooms', rooms);
+      console.log("before, rooms", rooms);
       delete rooms[socket.roomId].players[socket.id];
-      console.log('after, rooms', rooms);
+      console.log("after, rooms", rooms);
     }
     io.emit("disconnect", socket.id);
   });
 
   socket.on("playerMovement", movementData => {
-    const { x, y, socketId, roomId, direction } = movementData;
+    const { x, y, socketId, roomId, direction, big } = movementData;
     player = rooms[roomId].players[socketId];
     player.x = x;
     player.y = y;
